b832366182d26749f8c453eb693551ad73959184
0885787 <0885787@hr.nl>
2017-05-30 17:15:17 +0200

    Versie 1.2

---
+++ 1.2/Assets/Scripts/Player Scripts/PlayerPortalCollision.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerPortalCollision : MonoBehaviour {
+
+    // Use this for initialization
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+	}
+
+    void OnCollisionStay2D(Collision2D collision)
+    {
+        if (gameObject.tag == "Player1" && collision.gameObject.tag == "RedPortal")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+
+        if (gameObject.tag == "Player2" && collision.gameObject.tag == "YellowPortal")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+
+        if (gameObject.tag == "Player3" && collision.gameObject.tag == "GreenPortal")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+
+        if (gameObject.tag == "Player4" && collision.gameObject.tag == "BluePortal")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+
+        if (gameObject.tag == "OrangeSlime" && collision.gameObject.tag == "OrangePortal")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+        else if (gameObject.tag == "OrangeSlime2" && collision.gameObject.tag == "OrangePortal")
+        {
+            Physics2D.IgnoreCollision(collision.gameObject.GetComponent<Collider2D>(), GetComponent<Collider2D>());
+        }
+        else
+        {
+            
+        }
+
+        // Destroys the Orange Portal if anyone has the key
+        if (gameObject.GetComponent<PlayerController>().hasKey == true && collision.gameObject.tag == "OrangePortal")
+        {
+            Destroy(collision.gameObject);
+        }
+    }
+}
+
---
+++ 1.2/Assets/Scripts/Player Scripts/PressurePlate.cs
@@ -0,0 +1,184 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PressurePlate : MonoBehaviour {
+
+    //public GameObject[] gameObjects;
+    public GameObject gameObjects;
+
+    public Transform 	redPortal;
+    public Transform 	redPortalSwitch;
+    public Transform 	yellowPortal;
+    public Transform 	yellowPortalSwitch;
+    public Transform 	greenPortal;
+    public Transform 	greenPortalSwitch;
+    public Transform 	bluePortal;
+    public Transform 	bluePortalSwitch;
+	public AudioSource 	soundSource;
+	public AudioClip 	Sound;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (gameObject.tag == "PressurePlate" && collision.gameObject.tag == "Player1")
+        {
+            SwitchRedPortal();
+			soundSource.PlayOneShot(Sound);
+        }
+        if (gameObject.tag == "PressurePlate" && collision.gameObject.tag == "Player2")
+        {
+            SwitchYellowPortal();
+			soundSource.PlayOneShot(Sound);
+
+        }
+        if (gameObject.tag == "PressurePlate" && collision.gameObject.tag == "Player3")
+        {
+            SwitchGreenPortal();
+			soundSource.PlayOneShot(Sound);
+
+        }
+        if (gameObject.tag == "PressurePlate" && collision.gameObject.tag == "Player4")
+        {
+            SwitchBluePortal();
+			soundSource.PlayOneShot(Sound);
+        }
+    }
+
+    void OnTriggerExit2D(Collider2D collision)
+    {
+        {
+            if (gameObject.tag == "PressurePlate" && collision.gameObject.tag == "Player1")
+            {
+                SwitchRedBack();
+            }
+            if (gameObject.tag == "PressurePlate" && collision.gameObject.tag == "Player2")
+            {
+                SwitchYellowBack();
+            }
+            if (gameObject.tag == "PressurePlate" && collision.gameObject.tag == "Player3")
+            {
+                SwitchGreenBack();
+            }
+            if (gameObject.tag == "PressurePlate" && collision.gameObject.tag == "Player4")
+            {
+                SwitchBlueBack();
+            }
+        }
+    }
+
+    void SwitchRedPortal()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("RedSwitch");
+            Destroy(gameObjects);
+            Instantiate(bluePortal, gameObjects.transform.position, gameObjects.transform.rotation);
+			
+    }
+    void SwitchRedBack()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("BluePortalSwitch");
+            Destroy(gameObjects);
+            Instantiate(redPortalSwitch, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+
+    void SwitchYellowPortal()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("YellowSwitch");
+            Destroy(gameObjects);
+            Instantiate(redPortal, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+    void SwitchYellowBack()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("RedPortalSwitch");
+            Destroy(gameObjects);
+            Instantiate(yellowPortalSwitch, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+
+    void SwitchGreenPortal()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("GreenSwitch");
+            Destroy(gameObjects);
+            Instantiate(yellowPortal, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+    void SwitchGreenBack()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("YellowPortalSwitch");
+            Destroy(gameObjects);
+            Instantiate(greenPortalSwitch, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+
+    void SwitchBluePortal()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("BlueSwitch");
+            Destroy(gameObjects);
+            Instantiate(greenPortal, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+    void SwitchBlueBack()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("GreenPortalSwitch");
+            Destroy(gameObjects);
+            Instantiate(bluePortalSwitch, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+
+    /* void SwitchRedPortal()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("RedSwitch");
+            Destroy(gameObjects);
+            Instantiate(bluePortal, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+    void SwitchRedBack()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("BluePortalSwitch");
+            Destroy(gameObjects);
+            Instantiate(redPortalSwitch, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+
+    void SwitchYellowPortal()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("YellowSwitch");
+            Destroy(gameObjects);
+            Instantiate(redPortal, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+    void SwitchYellowBack()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("RedPortalSwitch");
+            Destroy(gameObjects);
+            Instantiate(yellowPortalSwitch, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+
+    void SwitchGreenPortal()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("GreenSwitch");
+            Destroy(gameObjects);
+            Instantiate(yellowPortal, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+    void SwitchGreenBack()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("YellowPortalSwitch");
+            Destroy(gameObjects);
+            Instantiate(greenPortalSwitch, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+
+    void SwitchBluePortal()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("BlueSwitch");
+            Destroy(gameObjects);
+            Instantiate(greenPortal, gameObjects.transform.position, gameObjects.transform.rotation);
+    }
+    void SwitchBlueBack()
+    {
+        gameObjects = GameObject.FindGameObjectWithTag("GreenPortalSwitch");
+            Destroy(gameObjects);
+            Instantiate(bluePortalSwitch, gameObjects.transform.position, gameObjects.transform.rotation);
+    }*/
+}
